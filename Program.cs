ed678c94a8395a3b09f9d972fd0ade3aaccb962b
Duki24ssj <huetepalaciojoseluis0@gmail.com>
2025-05-06 14:59:52 -0600

    Agregado el método Readkey() y cambie mensaje

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jose pelado");
+            Console.WriteLine("Duki");
+            Console.ReadKey();
         }
     }
 }
